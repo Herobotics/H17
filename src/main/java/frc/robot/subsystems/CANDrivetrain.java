2f1549466e4ee93f94ebf3d66b8a14bb6c0a10cc
Zero2Hero <herobotics2500@gmail.com>
2024-03-16 13:45:25 -0500

    got the imu working

--- src/main/java/frc/robot/subsystems/CANDrivetrain.java
+++ src/main/java/frc/robot/subsystems/CANDrivetrain.java
@@ -9,9 +9,11 @@ import static frc.robot.Constants.DrivetrainConstants.*;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
+import edu.wpi.first.wpilibj.ADIS16448_IMU;
 import edu.wpi.first.wpilibj.ADXRS450_Gyro;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
 import edu.wpi.first.wpilibj.drive.MecanumDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 /* This class declares the subsystem for the robot drivetrain if controllers are connected via CAN. Make sure to go to
@@ -26,7 +28,7 @@ public class CANDrivetrain extends SubsystemBase {
   different method calls. */
   MecanumDrive m_drivetrain;
   ADXRS450_Gyro gyro;
-
+  public static final ADIS16448_IMU imu = new ADIS16448_IMU();
   /*Constructor. This method is called when an instance of the class is created. This should generally be used to set up
    * member variables and perform any configuration or set up necessary on hardware.
    */
@@ -35,8 +37,7 @@ public class CANDrivetrain extends SubsystemBase {
     CANSparkMax leftRear = new CANSparkMax(kLeftRearID, MotorType.kBrushed);
     CANSparkMax rightFront = new CANSparkMax(kRightFrontID, MotorType.kBrushed);
     CANSparkMax rightRear = new CANSparkMax(kRightRearID, MotorType.kBrushed);
-    gyro = new ADXRS450_Gyro();
-
+    // gyro = new ADXRS450_Gyro();
     /*Sets current limits for the drivetrain motors. This helps reduce the likelihood of wheel spin, reduces motor heating
      *at stall (Drivetrain pushing against something) and helps maintain battery voltage under heavy demand */
     leftFront.setSmartCurrentLimit(kCurrentLimit);
@@ -63,14 +64,19 @@ m_drivetrain = new MecanumDrive(leftFront, leftRear, rightFront,rightRear);
    * and a rotation about the Z (turning the robot about it's center) and uses these to control the drivetrain motors */
   public void drive(double speed_x, double speed_y, double rotation) {
     //m_drivetrain.driveCartesian(speed_x, speed_y, rotation, gyro.getRotation2d());
+    SmartDashboard.putNumber("AngleX", CANDrivetrain.imu.getGyroAngleX());
+    SmartDashboard.putNumber("AngleY", CANDrivetrain.imu.getGyroAngleY());
+    SmartDashboard.putNumber("AngleZ", CANDrivetrain.imu.getGyroAngleZ());
     m_drivetrain.driveCartesian(speed_x, speed_y, rotation);
   }
 
   public void GyroCalibrate() {
-    gyro.calibrate();
+    // gyro.calibrate();
+    // imu.calibrate();
   }
   public void GyroReset() {
-    gyro.reset();
+    // gyro.reset();
+    // imu.reset();
   } 
 
   @Override
