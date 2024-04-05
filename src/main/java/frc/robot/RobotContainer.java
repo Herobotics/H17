7df7fd875089ab2ff0597750a9175bb65ff24463
Zero2Hero <herobotics2500@gmail.com>
2024-04-04 19:54:46 -0500

    Attempting to fix intake command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -16,6 +16,7 @@ import frc.robot.Constants.DrivetrainConstants;
 import frc.robot.Constants.LauncherConstants;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.commands.Autos;
+import frc.robot.commands.GroundIntake;
 import frc.robot.commands.LaunchNote;
 import frc.robot.commands.PrepareLaunch;
 import frc.robot.subsystems.CANDrivetrain;
@@ -98,10 +99,8 @@ public class RobotContainer {
     m_driverController.leftTrigger().whileTrue(m_launcher.getIntakeCommand());
     m_operatorController.leftTrigger().whileTrue(m_launcher.getIntakeCommand());
 
-    m_driverController.rightTrigger().whileTrue(m_launcher.getGroundIntakeCommand());
-    m_driverController.rightBumper().whileTrue(m_launcher.getGroundIntakeCommand());
-    m_operatorController.rightTrigger().whileTrue(m_launcher.getGroundIntakeCommand());
-    m_operatorController.rightBumper().whileTrue(m_launcher.getGroundIntakeCommand());
+    m_driverController.rightTrigger().whileTrue(new GroundIntake(m_launcher));
+    m_operatorController.rightTrigger().whileTrue(new GroundIntake(m_launcher));
 
     m_launcher.setDefaultCommand(
         new RunCommand(
--- src/main/java/frc/robot/commands/GroundIntake.java
+++ src/main/java/frc/robot/commands/GroundIntake.java
@@ -8,20 +8,49 @@ import static frc.robot.Constants.LauncherConstants.*;
 
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.Commands;
-import frc.robot.subsystems.CANDrivetrain;
 import frc.robot.subsystems.PWMLauncher;
-import frc.robot.Constants.DrivetrainConstants;
+import frc.robot.Constants.LauncherConstants;
 
 // import frc.robot.subsystems.CANLauncher;
 
-/*This is an example of creating a command as a class. The base Command class provides a set of methods that your command
- * will override.
- */
 public class GroundIntake extends Command {
-  /** Creates a new LaunchNote. */
+  PWMLauncher m_launcher;
+  /** Creates a new PrepareLaunch. */
   public GroundIntake(PWMLauncher launcher) {
-    return launcher.getMiddleGroundSpinupCommand()
-                .withTimeout(kIntakeDelay)
-                .andThen(launcher.getFullGroundIntakeCommand());
-    }
+    // save the launcher system internally
+    m_launcher = launcher;
+
+    // indicate that this command requires the launcher system
+    addRequirements(m_launcher);
+  }
+
+  // Called when the command is initially scheduled.
+  @Override
+  public void initialize() {
+    m_launcher.getMiddleGroundSpinupCommand()
+            .withTimeout(kIntakeDelay)
+            .andThen(m_launcher.getFullGroundIntakeCommand());
+  }
+
+  // Called every time the scheduler runs while the command is scheduled.
+  @Override
+  public void execute() {
+    // There is nothing we need this command to do on each iteration. You could remove this method
+    // and the default blank method
+    // of the base class will run.
+  }
+
+  // Called once the command ends or is interrupted.
+  @Override
+  public void end(boolean interrupted) {
+    // Do nothing when the command ends. The launch wheel needs to keep spinning in order to launch
+  }
+
+  // Returns true when the command should end.
+  @Override
+  public boolean isFinished() {
+    // Always return false so the command never ends on it's own. In this project we use a timeout
+    // decorator on the command to end it.
+    return false;
+  }
 }
